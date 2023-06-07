4e39f3b2afb980094b758bb5f1c78ea83375f643
Okagame <135771335+Okagame@users.noreply.github.com>
2023-06-06 20:36:57 -0400

    Update background.js

--- DeleteLastHour/background.js
+++ DeleteLastHour/background.js
@@ -1,32 +1,29 @@
+chrome.runtime.onInstalled.addListener(() => {
+  // Service worker registration code here
+});
+
 chrome.action.onClicked.addListener(async () => {
   try {
-    await chrome.browsingData.remove(
-      {
-        since: Date.now() - 3600000, // One hour ago
-      },
-      {
-        cache: true,
-        cookies: true,
-        downloads: true,
-        formData: true,
-        history: true,
-        indexedDB: true,
-        localStorage: true,
-        pluginData: true,
-        passwords: true,
-        serviceWorkers: true,
-        webSQL: true,
-      }
-    );
+    await chrome.browsingData.remove({
+      since: Date.now() - 3600000, // One hour ago
+    }, {
+      cache: true,
+      cookies: true,
+      downloads: true,
+      formData: true,
+      history: true,
+      indexedDB: true,
+      localStorage: true,
+      pluginData: true,
+      passwords: true,
+      serviceWorkers: true,
+      webSQL: true,
+    });
 
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
       const tab = tabs[0];
       if (tab) {
-        const { url, favIconUrl } = tab;
         await displayFeedback(tab.id, "Browsing data removed for the last hour");
-        if (favIconUrl && !favIconUrl.startsWith("chrome://")) {
-          await updateTabFavicon(tab.id, favIconUrl);
-        }
       } else {
         console.error("Unable to find active tab");
       }
@@ -56,15 +53,5 @@ async function displayFeedback(tabId, message) {
       }, 3000);
     },
     args: [message],
-  });
-}
-
-async function updateTabFavicon(tabId, faviconUrl) {
-  await chrome.tabs.update(tabId, { favIconUrl: faviconUrl });
-    const updatedTab = {
-      ...tab,
-      favIconUrl: faviconUrl,
-    };
-    chrome.tabs.update(updatedTab);
   });
 }
